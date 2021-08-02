df1313035e6409bcca900bdccfa587ae744c6abb
KimuraRyuya <toshi0931@icloud.com>
2021-08-02 10:06:39 +0900

    no message

--- Source/graduation_project/UI/CrossHair/BaseCrossHair.cpp
+++ Source/graduation_project/UI/CrossHair/BaseCrossHair.cpp
@@ -9,7 +9,7 @@ ABaseCrossHair::ABaseCrossHair()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	int a = 0;
 }
 
 // Called when the game starts or when spawned
