1a3473c4477f6d1dd51e241363898ad241ec4d95
MITSUI <m.y.31motsu2@gmail.com>
2021-11-24 16:30:08 +0900

    Minimon　未完成

--- Source/graduation_project/graduation_project.Build.cs
+++ Source/graduation_project/graduation_project.Build.cs
@@ -8,7 +8,7 @@ public class graduation_project : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "UMG", "Niagara" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "UMG", "Niagara", "AIModule" });
 		PublicDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore", "Json", "JsonUtilities", "GameplayTasks", "Paper2D" });
 	}
 }
